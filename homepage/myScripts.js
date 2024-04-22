97341dab6ff68125f07dbc3f8725cee0b908087c
swirlybbeans <140016106+swirlybbeans@users.noreply.github.com>
2024-04-22 11:11:45 -0500

    4-22

---
+++ homepage/myScripts.js
@@ -0,0 +1,19 @@
+//CLOCK SCRIPT
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock').innerHTML =  h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+   }
+   
+   function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+   }
+//WEATHER
+
+!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
